93a3e9720d9bbf5ca7ee6a3daa5555ce650267d8
chowdhu <chowdhu@us.ibm.com>
2018-02-11 05:38:21 +0000

    Tomcat updates

---
+++ 2_Tomcat/cookbook/tomcat/README.md
@@ -0,0 +1,26 @@
+# TOMCAT
+Use this Chef recipe to successfully install Tomcat on a RHEL-based system.
+
+# About this recipe
+Use this Chef recipe to successfully install Tomcat on a RHEL-based system.
+
+# Prerequistes
+* Provision a RHEL system(This will act as a node where we will install Tomcat)
+* Hosted chef or Chef server  
+* Workstation with chefDK installed and configured to talk to the Chef Server
+
+# Instructions
+Perform the following on the workstation
+
+* git clone https://github.com/chowdhu/workshops.git
+* cd workshops/2_Tomcat/cookbook
+* Upload the recipe to the Chef repo
+	* knife upload cookbook tomcat
+* Retrieve cookbook list and validate
+	* knife cookbook list
+* Bootstrap the provisioned node using key based authentication
+	* knife bootstrap <34.229.215.61> --ssh-user <ec2-user> --sudo --identity-file <~/.ssh/awskey.pem> --node-name <node1-rhel-tomcat> --run-list <'recipe[tomcat]'>( Make sure you change the ipaddress, ssh-user and identity file to match your environment)
+* Bootstrap the provisioned node using password authentication
+	* knife bootstrap <ipaddress> --ssh-user <USER> --ssh-password <password> --sudo --use-sudo-password --node-name <node1-centos-tomcat> --run-list <'recipe[tomcat]'>
+* Test it again
+	* knife ssh 'name:node1-rhel-mongo' 'sudo chef-client' --ssh-user ec2-user --identity-file ~/.ssh/awskey.pem
---
+++ 2_Tomcat/cookbook/tomcat/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook:: tomcat
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+#Install the wget and java packages
+['wget','java-1.7.0-openjdk-devel'].each do |p|
+  package p do
+    action :install
+  end
+end
+
+#Create a group called tomcat
+group 'tomcat'
+
+#Create a user called tomcat with group tomcat
+user 'tomcat' do
+  group 'tomcat'
+  shell '/bin/nologin'
+  home '/opt/tomcat'
+  manage_home  false
+end
+
+#This bash script will download the tar file untars it and modifies permissions of the directory
+bash 'install_tomcat' do
+  user 'ec2-user'
+  cwd '/tmp'
+  code <<-EOH
+  sudo mkdir -p /opt/tomcat
+  sudo wget -c  http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.27/bin/apache-tomcat-8.5.27.tar.gz
+  sudo tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
+  sudo chgrp -R tomcat /opt/tomcat
+  sudo chmod -R g+r /opt/tomcat/conf
+  sudo chmod g+x /opt/tomcat/conf
+  sudo chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/
+  EOH
+end
+
+#Create a service with the contents of the template
+template '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service.erb'
+end
+
+#Start and enable tomcat service on startup
+#Enable a service after a restart or reload
+service "tomcat" do
+  supports :restart => true, :reload => true
+  action [:enable,:start]
+end
