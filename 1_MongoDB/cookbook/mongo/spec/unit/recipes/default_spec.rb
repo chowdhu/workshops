516172126cfe463c017bfd225879dc0747bc124c
chowdhu <chowdhu@us.ibm.com>
2018-02-14 04:06:08 +0000

    Added unit tests

--- 1_MongoDB/cookbook/mongo/spec/unit/recipes/default_spec.rb
+++ 1_MongoDB/cookbook/mongo/spec/unit/recipes/default_spec.rb
@@ -7,16 +7,32 @@
 require 'spec_helper'
 
 describe 'mongo::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'Unit tests on RHEL7.4' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'redhat', version: '7.3')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
+
+   it 'Test: create yum repo for mongo' do
+	expect(chef_run).to create_template('/etc/yum.repos.d/mongodb.repo')
+   end
+
+   it 'Test: Install mongo' do
+	expect(chef_run).to install_package('mongodb-org')
+   end
+
+   it 'Test: mongo service started' do
+	expect(chef_run).to start_service('mongod')
+   end
+
+   it 'Test: mongo service enabled' do
+	expect(chef_run).to enable_service('mongod')
+   end
+end
 end
--- 2_Tomcat/cookbook/tomcat/spec/unit/recipes/default_spec.rb
+++ 2_Tomcat/cookbook/tomcat/spec/unit/recipes/default_spec.rb
@@ -7,7 +7,7 @@
 require 'spec_helper'
 
 describe 'tomcat::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'Unit tests on RHEL7.4' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
@@ -18,5 +18,30 @@ describe 'tomcat::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+   it 'Test: Include java package' do
+	expect(chef_run).to include_recipe('tomcat::java')
+   end
+
+   it 'Test: Install wget' do
+	expect(chef_run).to install_package('wget')
+   end
+
+   it 'Test: Check group tomcat' do
+	expect(chef_run).to create_group('tomcat')
+   end
+
+   it 'Test: Check user tomcat' do
+	expect(chef_run).to create_user('tomcat')
+   end
+
+   it 'Test: Create a service' do
+	expect(chef_run).to create_template('/etc/systemd/system/tomcat.service')
+   end
+
+   it 'Test: service started' do
+	expect(chef_run).to start_service('tomcat')
+   end
+
   end
 end
